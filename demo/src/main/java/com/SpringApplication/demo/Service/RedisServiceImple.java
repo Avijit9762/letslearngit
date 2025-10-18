32500729ed21d36368d01219f88e36e0cf544ebb
avijit <avijithait@zohomail.in>
2025-10-18 21:31:30 +0530

    ok

---
+++ demo/src/main/java/com/SpringApplication/demo/Service/RedisServiceImple.java
@@ -0,0 +1,23 @@
+package com.SpringApplication.demo.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisServiceImple {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+
+
+    public void saveData(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public String getData(String key) {
+        return (String) redisTemplate.opsForValue().get(key);
+    }
+}
